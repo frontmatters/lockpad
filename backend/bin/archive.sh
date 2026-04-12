f0069096ba28b8edaaf4b96b3bdfe29bb611c9c9
Athlon1600 <Athlon2400@gmail.com>
2026-04-12 11:37:46 -0700

    updated backup script

--- backend/bin/archive.sh
+++ backend/bin/archive.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-set -eux
+set -eu
 
 # Define directories
 BIN_DIR="$(dirname "$(readlink -f "$0")")"
 BACKEND_DIR="$(dirname "$BIN_DIR")"
 STORAGE_DIR="$BACKEND_DIR/storage"
-ARCHIVE_DIR="$BACKEND_DIR/public"
+BACKUPS_DIR="$BACKEND_DIR/public/backups"
 
 if [ ! -d "$STORAGE_DIR" ]; then
   echo "storage directory ($STORAGE_DIR) not found."
   exit 1
 fi
 
-if [ ! -d "$ARCHIVE_DIR" ]; then
-  echo "public directory '$ARCHIVE_DIR' not found."
+if [ ! -d "$BACKUPS_DIR" ]; then
+  echo "public directory ($BACKUPS_DIR) not found."
   exit 1
 fi
 
-# change to storage directory just in case
-tar -czf archive.tar.gz -C "$BACKEND_DIR" storage && mv archive.tar.gz "$ARCHIVE_DIR"
+# change to backend directory before compression
+tar -czf archive.tar.gz -C "$BACKEND_DIR" storage
 
-echo -e "\e[32m\u2705 Archive created in $ARCHIVE_DIR \e[0m"
+## make a dated copy of it
+cp archive.tar.gz "$BACKUPS_DIR/archive_$(date +%Y%m%d).tar.gz"
+mv archive.tar.gz "$BACKUPS_DIR"
+
+echo -e "\e[32m\u2705 Archive created in $BACKUPS_DIR \e[0m"
