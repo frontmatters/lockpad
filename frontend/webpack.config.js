b8256167871dd0a015c92615aeeab4e35f672512
Athlon1600 <Athlon2400@gmail.com>
2026-04-12 13:24:16 -0700

    prerender homepage html

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -81,7 +81,7 @@ if (isProd) {
 
     // requires extra dependencies
     // error while loading shared libraries: libX11-xcb.so.1: cannot open shared object file: No such file or directory
-    // webpackConfig.plugins.push(prerender);
+    webpackConfig.plugins.push(prerender);
 }
 
 module.exports = webpackConfig;
